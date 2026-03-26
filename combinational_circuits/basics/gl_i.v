25817cb6d6807f2a9e975497630afc4588d1da8b
Keerthana <115525536+Keerthyii@users.noreply.github.com>
2026-03-26 17:25:06 +0530

    Add files via upload

---
+++ combinational_circuits/basics/gl_i.v
@@ -0,0 +1,23 @@
+module gl_g(
+    input A,
+    input B,
+    output AND1,
+    output OR1,
+    output NAND1,
+    output NOR1,
+    output XOR1,
+    output XNOR1,
+    output NOTA,
+    output NOTB
+);
+
+and   g1 (AND1, A, B);
+or    g2 (OR1, A, B);
+nand  g3 (NAND1, A, B);
+nor   g4 (NOR1, A, B);
+xor   g5 (XOR1, A, B);
+xnor  g6 (XNOR1, A, B);
+not   g7 (NOTA, A);
+not   g8 (NOTB, B);
+
+endmodule
---
+++ combinational_circuits/basics/tb_gl_i.v
@@ -0,0 +1,44 @@
+module gl_g_tb;
+
+reg A;
+reg B;
+
+wire AND1;
+wire OR1;
+wire NAND1;
+wire NOR1;
+wire XOR1;
+wire XNOR1;
+wire NOTA;
+wire NOTB;
+
+gl_g uut(
+A,
+B,
+AND1,
+OR1,
+NAND1,
+NOR1,
+XOR1,
+XNOR1,
+NOTA,
+NOTB
+);
+
+initial
+begin
+
+A = 0; B = 0; #10;
+A = 0; B = 1; #10;
+A = 1; B = 0; #10;
+A = 1; B = 1; #10;
+
+end
+
+initial
+begin
+$monitor("A=%b B=%b AND=%b OR=%b NAND=%b NOR=%b XOR=%b XNOR=%b NOTA=%b NOTB=%b",
+A,B,AND1,OR1,NAND1,NOR1,XOR1,XNOR1,NOTA,NOTB);
+end
+
+endmodule
