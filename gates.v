2098be6749982cde070319baf2348086fe186f36
Keerthana <115525536+Keerthyii@users.noreply.github.com>
2026-03-26 15:42:40 +0530

    Delete gates.v

--- gates.v
+++
@@ -1,9 +0,0 @@
-module all_gates_df(input a,b,output andd,orr,nandd,norr,xorr,xnorr,notta);
-assign andd=a&b;
-assign orr= a|b;
-assign nandd=~(a&b);
-assign norr =~(a|b);
-assign xorr =a^b;
-assign xnorr=~(a|b);
-assign notta=~a;
-endmodule 
