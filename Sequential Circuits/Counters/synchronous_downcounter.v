fb114b7c0d0846d3a5fb8b0362d0064785168c51
Keerthana <115525536+Keerthyii@users.noreply.github.com>
2026-04-25 10:41:00 +0530

    Add files via upload

---
+++ Sequential Circuits/Counters/synchronous_downcounter.v
@@ -0,0 +1,10 @@
+module synchronous_downcounter(input clk,rst,output  reg[2:0]q);
+always @(posedge clk)
+begin
+if(rst)
+q<=3'b000;
+else
+q<=q-1;
+end
+endmodule
+
---
+++ Sequential Circuits/Counters/tb_synchronous_downcounter.v
@@ -0,0 +1,22 @@
+module tb_synchronous_downcounter;
+reg clk,rst;
+wire [2:0] q;
+synchronous_downcounter uut(clk,rst,q);
+initial 
+begin
+clk=0;
+forever #5 clk=~clk;
+end
+initial
+begin
+$monitor("clk %b rst %b q %b",clk,rst,q);
+#10;
+rst=1;
+#10;
+
+#15;
+rst=0;
+#100;
+$finish;
+end
+endmodule 
