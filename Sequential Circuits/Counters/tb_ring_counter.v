e91fb12341be68bb6ae56a26361d5cab6e895efe
Keerthana <115525536+Keerthyii@users.noreply.github.com>
2026-04-26 23:31:19 +0530

    Add files via upload

---
+++ Sequential Circuits/Counters/tb_ring_counter.v
@@ -0,0 +1,26 @@
+module tb_ring_counter;
+
+reg clk;
+reg rst;
+wire [3:0] q;
+ring_counter uut (
+    .clk(clk),
+    .rst(rst),
+    .q(q)
+);
+
+always #5 clk = ~clk;
+
+initial begin
+    clk = 0;
+    rst = 1;
+    #10;
+    rst = 0;
+    #80;
+    $finish;
+end
+initial begin
+    $monitor("Time=%0t | rst=%b | q=%b", $time, rst, q);
+end
+
+endmodule
