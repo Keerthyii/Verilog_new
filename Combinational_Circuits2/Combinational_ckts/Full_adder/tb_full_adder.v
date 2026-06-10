e811a07b004b4c8eec61fa409d96049bc030aab7
Keerthana <115525536+Keerthyii@users.noreply.github.com>
2026-06-10 11:32:11 +0530

    Add files via upload

---
+++ Combinational_Circuits2/Combinational_ckts/Full_adder/tb_full_adder.v
@@ -0,0 +1,29 @@
+module tb_full_adder;
+reg a,b,cin;
+wire s,cout;
+full_adder dut (
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .s(s),
+    .cout(cout)
+);
+
+initial begin
+
+    $monitor("a=%b b=%b cin=%b | s=%b cout=%b",
+              a, b, cin, s, cout);
+
+    a=0; b=0; cin=0; #10;
+    a=0; b=0; cin=1; #10;
+    a=0; b=1; cin=0; #10;
+    a=0; b=1; cin=1; #10;
+    a=1; b=0; cin=0; #10;
+    a=1; b=0; cin=1; #10;
+    a=1; b=1; cin=0; #10;
+    a=1; b=1; cin=1; #10;
+
+    $finish;
+end
+
+endmodule
